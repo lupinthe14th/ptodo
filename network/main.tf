ac14485df5dd67d861d053b7d0826bae3e9cc2c4
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2020-12-19 23:00:12 +0900

    improve: build production mode

--- network/main.tf
+++ network/main.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "ptodo-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "20.0.0.0/16"
 
   azs              = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
-  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  public_subnets   = ["20.0.101.0/24", "20.0.102.0/24", "20.0.103.0/24"]
+  database_subnets = ["20.0.201.0/24", "20.0.202.0/24", "20.0.203.0/24"]
 
   create_database_subnet_group = false
 
